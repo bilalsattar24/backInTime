607a0f53658daf67a58372f232786721d36be855
alexanderDoria <adoria@csumb.edu>
2016-12-03 09:24:14 -0800

    included js file(s)

---
+++ dev/index.js
@@ -0,0 +1,10 @@
+<SCRIPT LANGUAGE="JavaScript">
+
+test = new Date()
+month = test.getMonth()
+month = (month * 1) + 1
+day = test.getDate()
+year = test.getFullYear()
+document.write(" ",month,"/",day,"/",year," ")
+// end hiding script from old browsers -->
+</SCRIPT>
